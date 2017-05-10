80f83030eb33428f44d92a8435f002458d2c924d
Richard Paul <richard@primarysite.net>
2017-05-10 09:08:21 +0100

    * Adding including local settings file into the main settings file and updating the local.py

--- webhook/webhook/local.py
+++ webhook/webhook/local.py
@@ -1,3 +1,7 @@
+# Settings that you'll want to override in production
+# ALLOWED_HOSTS []
+# DEBUG = True
+
 GITHUB_WEBHOOK_KEY = 'a-long-string-goes-here'
 R10K_BIN = '/bin/r10k'
 R10K_CONFDIR = '/etc/r10k'
