f8a159fed2286e0042fccced2607deba40428a75
Richard Paul <richard@primarysite.net>
2017-10-09 12:07:39 +0100

    Just comment out all the override settings and blank them

--- webhook/webhook/local.py
+++ webhook/webhook/local.py
@@ -1,12 +1,11 @@
 # Settings that you'll want to override in production
 # ALLOWED_HOSTS []
 # DEBUG = False
-
-GITHUB_WEBHOOK_KEY = 'a-long-string-goes-here'
-R10K_BIN = '/bin/r10k'
-R10K_CONFDIR = '/etc/r10k'
-R10K_USER = 'r10k'
-HOOK_SCRIPT = '/usr/local/bin/hook.sh'
-ENVIRONMENT_ENV = ''
-PUPPET_USER = 'puppet'
-ENC_DIR = '/etc/enc'
+# GITHUB_WEBHOOK_KEY = ''
+# R10K_BIN = ''
+# R10K_CONFDIR = ''
+# R10K_USER = ''
+# HOOK_SCRIPT = ''
+# ENVIRONMENT_ENV = ''
+# PUPPET_USER = ''
+# ENC_DIR = ''
